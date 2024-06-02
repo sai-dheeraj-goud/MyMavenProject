f6302a9c151ca72965e1e0bc4de24aaaab99511e
sai-dheeraj-goud <dheeraj.sai17@gmail.com>
2024-06-02 22:33:00 +0530

    02-06-2024 22:32

--- src/test/java/TestUtilities/TC_01.java
+++ src/test/java/TestUtilities/TC_01.java
@@ -10,7 +10,7 @@ import WebUtilities.LoginPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TC_01 {
-	//dheeraj
+	//dheeraj david
 	
 	static WebDriver driver;
 	public static void main(String[] args) 
