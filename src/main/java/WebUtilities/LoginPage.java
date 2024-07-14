20de842a713323f8bf2b490680eb6fce4bf8544f
sai-dheeraj-goud <dheeraj.sai17@gmail.com>
2024-07-14 16:05:34 +0530

    changed in loginjava added 1 in my name and resolved conflit-2

--- src/main/java/WebUtilities/LoginPage.java
+++ src/main/java/WebUtilities/LoginPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-//sai dheeraj goud mahendra
+//sai dheeraj goud mahendra 123
 
 public class LoginPage {
 	
